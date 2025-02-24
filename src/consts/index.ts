99a10d0ceb84b794f311037dd4c9c0a1e7e4351c
Reid Harrison <reid.al.harrisson@gmail.com>
2025-02-24 21:56:53 +0100

    feat: add data chart

---
+++ src/consts/index.ts
@@ -0,0 +1,802 @@
+export const data = {
+  "0": {
+    Count: 9,
+    Color: "#000104ff",
+  },
+  "1": {
+    Count: 4,
+    Color: "#000104ff",
+  },
+  "2": {
+    Count: 4,
+    Color: "#010306ff",
+  },
+  "3": {
+    Count: 5,
+    Color: "#050505ff",
+  },
+  "4": {
+    Count: 5,
+    Color: "#060606ff",
+  },
+  "5": {
+    Count: 37,
+    Color: "#050708ff",
+  },
+  "6": {
+    Count: 67,
+    Color: "#060809ff",
+  },
+  "7": {
+    Count: 199,
+    Color: "#070a0dff",
+  },
+  "8": {
+    Count: 128,
+    Color: "#080b0eff",
+  },
+  "9": {
+    Count: 96,
+    Color: "#090c0fff",
+  },
+  "10": {
+    Count: 80,
+    Color: "#0a0d10ff",
+  },
+  "11": {
+    Count: 92,
+    Color: "#0b0e11ff",
+  },
+  "12": {
+    Count: 28,
+    Color: "#0d1013ff",
+  },
+  "13": {
+    Count: 16,
+    Color: "#0c1216ff",
+  },
+  "14": {
+    Count: 11,
+    Color: "#111316ff",
+  },
+  "15": {
+    Count: 5,
+    Color: "#101316ff",
+  },
+  "16": {
+    Count: 2,
+    Color: "#121518ff",
+  },
+  "17": {
+    Count: 2,
+    Color: "#141619ff",
+  },
+  "18": {
+    Count: 2,
+    Color: "#14171eff",
+  },
+  "19": {
+    Count: 1,
+    Color: "#16191cff",
+  },
+  "20": {
+    Count: 1,
+    Color: "#1a191bff",
+  },
+  "21": {
+    Count: 1,
+    Color: "#1a1c1fff",
+  },
+  "22": {
+    Count: 1,
+    Color: "#1a1c1dff",
+  },
+  "23": {
+    Count: 0,
+    Color: "#1b1e21ff",
+  },
+  "24": {
+    Count: 0,
+    Color: "#1c1f22ff",
+  },
+  "25": {
+    Count: 0,
+    Color: "#1d2023ff",
+  },
+  "26": {
+    Count: 0,
+    Color: "#1f2225ff",
+  },
+  "27": {
+    Count: 0,
+    Color: "#212326ff",
+  },
+  "28": {
+    Count: 0,
+    Color: "#222425ff",
+  },
+  "29": {
+    Count: 0,
+    Color: "#242629ff",
+  },
+  "30": {
+    Count: 0,
+    Color: "#242827ff",
+  },
+  "31": {
+    Count: 0,
+    Color: "#21292aff",
+  },
+  "32": {
+    Count: 0,
+    Color: "#282a2dff",
+  },
+  "33": {
+    Count: 0,
+    Color: "#292b2eff",
+  },
+  "34": {
+    Count: 0,
+    Color: "#2a2c2fff",
+  },
+  "35": {
+    Count: 0,
+    Color: "#2b2d2dff",
+  },
+  "36": {
+    Count: 0,
+    Color: "#2c2e31ff",
+  },
+  "37": {
+    Count: 0,
+    Color: "#2d3131ff",
+  },
+  "38": {
+    Count: 0,
+    Color: "#2e3134ff",
+  },
+  "39": {
+    Count: 0,
+    Color: "#313336ff",
+  },
+  "40": {
+    Count: 0,
+    Color: "#343335ff",
+  },
+  "41": {
+    Count: 0,
+    Color: "#323636ff",
+  },
+  "42": {
+    Count: 0,
+    Color: "#343637ff",
+  },
+  "43": {
+    Count: 0,
+    Color: "#36383bff",
+  },
+  "44": {
+    Count: 0,
+    Color: "#363a3aff",
+  },
+  "45": {
+    Count: 0,
+    Color: "#373a3dff",
+  },
+  "46": {
+    Count: 0,
+    Color: "#383b3eff",
+  },
+  "47": {
+    Count: 0,
+    Color: "#393c3fff",
+  },
+  "48": {
+    Count: 0,
+    Color: "#3c3e40ff",
+  },
+  "49": {
+    Count: 0,
+    Color: "#3b4042ff",
+  },
+  "50": {
+    Count: 0,
+    Color: "#3e4043ff",
+  },
+  "51": {
+    Count: 0,
+    Color: "#3e4243ff",
+  },
+  "52": {
+    Count: 0,
+    Color: "#404446ff",
+  },
+  "53": {
+    Count: 0,
+    Color: "#424447ff",
+  },
+  "54": {
+    Count: 0,
+    Color: "#424645ff",
+  },
+  "55": {
+    Count: 0,
+    Color: "#444849ff",
+  },
+  "56": {
+    Count: 0,
+    Color: "#46474dff",
+  },
+  "57": {
+    Count: 0,
+    Color: "#48494fff",
+  },
+  "58": {
+    Count: 0,
+    Color: "#464c4cff",
+  },
+  "59": {
+    Count: 0,
+    Color: "#494d4fff",
+  },
+  "60": {
+    Count: 0,
+    Color: "#4b4d4fff",
+  },
+  "61": {
+    Count: 0,
+    Color: "#4c4e50ff",
+  },
+  "62": {
+    Count: 0,
+    Color: "#4c5155ff",
+  },
+  "63": {
+    Count: 0,
+    Color: "#4e5253ff",
+  },
+  "64": {
+    Count: 0,
+    Color: "#4f5257ff",
+  },
+  "65": {
+    Count: 0,
+    Color: "#515453ff",
+  },
+  "66": {
+    Count: 0,
+    Color: "#535655ff",
+  },
+  "67": {
+    Count: 0,
+    Color: "#535758ff",
+  },
+  "68": {
+    Count: 0,
+    Color: "#54585aff",
+  },
+  "69": {
+    Count: 0,
+    Color: "#56585aff",
+  },
+  "70": {
+    Count: 0,
+    Color: "#585a5dff",
+  },
+  "71": {
+    Count: 0,
+    Color: "#585c5bff",
+  },
+  "72": {
+    Count: 0,
+    Color: "#5b5d60ff",
+  },
+  "73": {
+    Count: 0,
+    Color: "#5c5e63ff",
+  },
+  "74": {
+    Count: 0,
+    Color: "#5c6062ff",
+  },
+  "75": {
+    Count: 0,
+    Color: "#5f6166ff",
+  },
+  "76": {
+    Count: 0,
+    Color: "#5f6163ff",
+  },
+  "77": {
+    Count: 0,
+    Color: "#616366ff",
+  },
+  "78": {
+    Count: 0,
+    Color: "#646367ff",
+  },
+  "79": {
+    Count: 0,
+    Color: "#636567ff",
+  },
+  "80": {
+    Count: 0,
+    Color: "#62676bff",
+  },
+  "81": {
+    Count: 0,
+    Color: "#63686aff",
+  },
+  "82": {
+    Count: 0,
+    Color: "#656a6cff",
+  },
+  "83": {
+    Count: 0,
+    Color: "#696b6aff",
+  },
+  "84": {
+    Count: 0,
+    Color: "#696d6eff",
+  },
+  "85": {
+    Count: 0,
+    Color: "#6a6e6fff",
+  },
+  "86": {
+    Count: 0,
+    Color: "#6f6d78ff",
+  },
+  "87": {
+    Count: 0,
+    Color: "#6e6f71ff",
+  },
+  "88": {
+    Count: 0,
+    Color: "#6d7172ff",
+  },
+  "89": {
+    Count: 0,
+    Color: "#6f7375ff",
+  },
+  "90": {
+    Count: 0,
+    Color: "#6f7478ff",
+  },
+  "91": {
+    Count: 0,
+    Color: "#71767aff",
+  },
+  "92": {
+    Count: 0,
+    Color: "#717679ff",
+  },
+  "93": {
+    Count: 0,
+    Color: "#74787bff",
+  },
+  "94": {
+    Count: 0,
+    Color: "#75797cff",
+  },
+  "95": {
+    Count: 0,
+    Color: "#797a7cff",
+  },
+  "96": {
+    Count: 0,
+    Color: "#787c7fff",
+  },
+  "97": {
+    Count: 0,
+    Color: "#797d80ff",
+  },
+  "98": {
+    Count: 0,
+    Color: "#7d7e80ff",
+  },
+  "99": {
+    Count: 0,
+    Color: "#7c7f84ff",
+  },
+  "100": {
+    Count: 0,
+    Color: "#7c8185ff",
+  },
+  "101": {
+    Count: 0,
+    Color: "#808183ff",
+  },
+  "102": {
+    Count: 0,
+    Color: "#828483ff",
+  },
+  "103": {
+    Count: 0,
+    Color: "#818588ff",
+  },
+  "104": {
+    Count: 0,
+    Color: "#828687ff",
+  },
+  "105": {
+    Count: 0,
+    Color: "#85868aff",
+  },
+  "106": {
+    Count: 0,
+    Color: "#84898cff",
+  },
+  "107": {
+    Count: 0,
+    Color: "#858a8dff",
+  },
+  "108": {
+    Count: 0,
+    Color: "#898b8aff",
+  },
+  "109": {
+    Count: 0,
+    Color: "#898d90ff",
+  },
+  "110": {
+    Count: 0,
+    Color: "#8c8d91ff",
+  },
+  "111": {
+    Count: 0,
+    Color: "#8d8e90ff",
+  },
+  "112": {
+    Count: 0,
+    Color: "#8c9093ff",
+  },
+  "113": {
+    Count: 0,
+    Color: "#8d9194ff",
+  },
+  "114": {
+    Count: 0,
+    Color: "#8f9396ff",
+  },
+  "115": {
+    Count: 0,
+    Color: "#90939aff",
+  },
+  "116": {
+    Count: 0,
+    Color: "#909698ff",
+  },
+  "117": {
+    Count: 0,
+    Color: "#929699ff",
+  },
+  "118": {
+    Count: 0,
+    Color: "#97969bff",
+  },
+  "119": {
+    Count: 0,
+    Color: "#97989aff",
+  },
+  "120": {
+    Count: 0,
+    Color: "#969a9dff",
+  },
+  "121": {
+    Count: 0,
+    Color: "#989c9fff",
+  },
+  "122": {
+    Count: 0,
+    Color: "#989da3ff",
+  },
+  "123": {
+    Count: 0,
+    Color: "#9e9e9eff",
+  },
+  "124": {
+    Count: 0,
+    Color: "#9a9fa3ff",
+  },
+  "125": {
+    Count: 0,
+    Color: "#9da1a4ff",
+  },
+  "126": {
+    Count: 0,
+    Color: "#9ca3a5ff",
+  },
+  "127": {
+    Count: 0,
+    Color: "#9fa3a6ff",
+  },
+  "128": {
+    Count: 0,
+    Color: "#a0a6a8ff",
+  },
+  "129": {
+    Count: 0,
+    Color: "#a1a6aaff",
+  },
+  "130": {
+    Count: 0,
+    Color: "#a3a7aaff",
+  },
+  "131": {
+    Count: 0,
+    Color: "#a3a8acff",
+  },
+  "132": {
+    Count: 0,
+    Color: "#a4aaacff",
+  },
+  "133": {
+    Count: 0,
+    Color: "#a5aba9ff",
+  },
+  "134": {
+    Count: 0,
+    Color: "#aaacabff",
+  },
+  "135": {
+    Count: 0,
+    Color: "#a8aeb0ff",
+  },
+  "136": {
+    Count: 0,
+    Color: "#adafb0ff",
+  },
+  "137": {
+    Count: 0,
+    Color: "#acb0b1ff",
+  },
+  "138": {
+    Count: 0,
+    Color: "#adb2b0ff",
+  },
+  "139": {
+    Count: 0,
+    Color: "#afb3b8ff",
+  },
+  "140": {
+    Count: 0,
+    Color: "#afb4baff",
+  },
+  "141": {
+    Count: 0,
+    Color: "#b1b5b8ff",
+  },
+  "142": {
+    Count: 0,
+    Color: "#b1b6bbff",
+  },
+  "143": {
+    Count: 0,
+    Color: "#b4b8bbff",
+  },
+  "144": {
+    Count: 0,
+    Color: "#b5b9bcff",
+  },
+  "145": {
+    Count: 0,
+    Color: "#b5bac0ff",
+  },
+  "146": {
+    Count: 0,
+    Color: "#b8bcbfff",
+  },
+  "147": {
+    Count: 0,
+    Color: "#b9bdc0ff",
+  },
+  "148": {
+    Count: 0,
+    Color: "#babec3ff",
+  },
+  "149": {
+    Count: 0,
+    Color: "#bcc0c3ff",
+  },
+  "150": {
+    Count: 0,
+    Color: "#bcc1c7ff",
+  },
+  "151": {
+    Count: 0,
+    Color: "#bdc2c7ff",
+  },
+  "152": {
+    Count: 0,
+    Color: "#bdc4c8ff",
+  },
+  "153": {
+    Count: 0,
+    Color: "#c2c4c5ff",
+  },
+  "154": {
+    Count: 0,
+    Color: "#c2c7c5ff",
+  },
+  "155": {
+    Count: 0,
+    Color: "#c3c7caff",
+  },
+  "156": {
+    Count: 0,
+    Color: "#c4c8cbff",
+  },
+  "157": {
+    Count: 0,
+    Color: "#c4caccff",
+  },
+  "158": {
+    Count: 0,
+    Color: "#c6cbd0ff",
+  },
+  "159": {
+    Count: 0,
+    Color: "#c7ccd1ff",
+  },
+  "160": {
+    Count: 0,
+    Color: "#caced3ff",
+  },
+  "161": {
+    Count: 0,
+    Color: "#c8cfd3ff",
+  },
+  "162": {
+    Count: 0,
+    Color: "#cbd1d3ff",
+  },
+  "163": {
+    Count: 0,
+    Color: "#d0d1d5ff",
+  },
+  "164": {
+    Count: 0,
+    Color: "#d0d3d8ff",
+  },
+  "165": {
+    Count: 0,
+    Color: "#d1d5d3ff",
+  },
+  "166": {
+    Count: 0,
+    Color: "#d2d5d8ff",
+  },
+  "167": {
+    Count: 0,
+    Color: "#d3d6d9ff",
+  },
+  "168": {
+    Count: 0,
+    Color: "#d6d7dbff",
+  },
+  "169": {
+    Count: 0,
+    Color: "#d6d9deff",
+  },
+  "170": {
+    Count: 0,
+    Color: "#d4dbdfff",
+  },
+  "171": {
+    Count: 0,
+    Color: "#d8dcdaff",
+  },
+  "172": {
+    Count: 0,
+    Color: "#dadde0ff",
+  },
+  "173": {
+    Count: 0,
+    Color: "#dcdedfff",
+  },
+  "174": {
+    Count: 0,
+    Color: "#dae0e2ff",
+  },
+  "175": {
+    Count: 0,
+    Color: "#dbe0e5ff",
+  },
+  "176": {
+    Count: 0,
+    Color: "#dfe2e5ff",
+  },
+  "177": {
+    Count: 0,
+    Color: "#e0e3e6ff",
+  },
+  "178": {
+    Count: 0,
+    Color: "#dee6e7ff",
+  },
+  "179": {
+    Count: 0,
+    Color: "#e1e7e9ff",
+  },
+  "180": {
+    Count: 0,
+    Color: "#e4e7e8ff",
+  },
+  "181": {
+    Count: 0,
+    Color: "#e5e8ebff",
+  },
+  "182": {
+    Count: 0,
+    Color: "#e6e9eeff",
+  },
+  "183": {
+    Count: 0,
+    Color: "#e6eceeff",
+  },
+  "184": {
+    Count: 0,
+    Color: "#e7edefff",
+  },
+  "185": {
+    Count: 0,
+    Color: "#ecedf3ff",
+  },
+  "186": {
+    Count: 0,
+    Color: "#ebefefff",
+  },
+  "187": {
+    Count: 0,
+    Color: "#eef0efff",
+  },
+  "188": {
+    Count: 0,
+    Color: "#eff0f4ff",
+  },
+  "189": {
+    Count: 1,
+    Color: "#edf3f5ff",
+  },
+  "190": {
+    Count: 1,
+    Color: "#eef4f6ff",
+  },
+  "191": {
+    Count: 1,
+    Color: "#f2f6f6ff",
+  },
+  "192": {
+    Count: 1,
+    Color: "#f4f6f7ff",
+  },
+  "193": {
+    Count: 1,
+    Color: "#f7f7f9ff",
+  },
+  "194": {
+    Count: 1,
+    Color: "#f7f9f8ff",
+  },
+  "195": {
+    Count: 1,
+    Color: "#f8faf9ff",
+  },
+  "196": {
+    Count: 2,
+    Color: "#f9fbfaff",
+  },
+  "197": {
+    Count: 3,
+    Color: "#f7fdffff",
+  },
+  "198": {
+    Count: 4,
+    Color: "#fbffffff",
+  },
+  "199": {
+    Count: 11,
+    Color: "#fcffffff",
+  },
+};
